19c5a7d475cacb98b5725e75087067a036d2cea0
Digamer001 <diegobezerra864@gmail.com>
2023-05-19 18:01:49 -0300

    Upadate

--- ex00015/script.js
+++ ex00015/script.js
@@ -9,13 +9,37 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement ('img')
+        img.setAttribute('id','foto')
         if (fsex[0].checked) {
             gênero = 'homem'
-            
+            if (idade > 10 && idade < 21){
+                //jovem
+                img.setAttribute('src','foto-jovem-m.png')
+            }else if(idade >= 21 && idade < 60){
+                //ADULTO
+                img.setAttribute('src','foto-adulto-m.png')
+                
+            }else if(idade > 60){
+                //velho
+                img.setAttribute('src','foto-idoso.png')
+            }
         }else if (fsex[1].checked){
             gênero = 'mulher'
+            if (idade > 10 && idade < 21){
+                //jovem
+                img.setAttribute('src','foto-jovem-f.png')
+            }else if(idade >= 21 && idade < 60){
+                //ADULTO
+                img.setAttribute('src','foto-adulto-f.png')
+                
+            }else if(idade > 60){
+                //velho
+                img.setAttribute('src','foto-idosa.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
