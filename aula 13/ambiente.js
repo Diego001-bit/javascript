6b324e7cd01b57fe6402174cfc895150ba7ee524
Digamer001 <diegobezerra864@gmail.com>
2023-05-21 17:15:13 -0300

    deu certo

---
+++ aula 13/ambiente.js
@@ -0,0 +1,18 @@
+console.log('Tudo bem?')
+
+var c = 1 
+do  {
+    console.log(`Passo ${c}`)
+    c++
+    
+} while (c <= 6)
+
+/*
+console.log('Tudo bem?')
+
+var c = 1 
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
